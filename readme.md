65217095232c0c23bd709aa3b9237109d7ee352e
Snir Sofer <snir.sofer3@gmail.com>
2019-02-20 14:33:24 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,54 @@
+# WP Live Code Embed
+Live code embed is a plugin for wordpress to embed Pastebin snippets, github gists, github profile, github repository and codepen.
+
+--
+##### Author: Snir Sofer
+##### Website: https://waf.co.il/
+##### Version: 1.0.0
+--
+## Syntax
+---
+#### Embed Github Profile
+![N|Solid](https://waf.co.il/addons/uploads/2019/02/screenshot-2.png)
+```
+[github_profile username="rackerlabs"]
+```
+---
+#### Embed Github Repository
+![N|Solid](https://waf.co.il/addons/uploads/2019/02/screenshot-1.png)
+```
+[github_repo id="rackerlabs/repose"] 
+```
+---
+#### Embed Github Gists
+![N|Solid](https://waf.co.il/addons/uploads/2019/02/screenshot-3.png)
+```
+[gist id="fac84b22c965a58d21510392b09d7254"]
+```
+---
+#### Embed Pastebin Snippet
+![N|Solid](https://waf.co.il/addons/uploads/2019/02/screenshot-4.png)
+```
+[pastebin id="5mD8Uc3m"]
+```
+---
+#### Embed CodePen Snippet
+![N|Solid](https://waf.co.il/addons/uploads/2019/02/screenshot-5.png)
+```
+[codepen id="evsuw"]
+```
+
+### Credits
+
+WP Live Code Embed uses a number of open source projects to work properly:
+
+* [JQuery] (https://jquery.com/) - [John Resig](https://johnresig.com/)
+* [Github Cards] - [lepture](https://github.com/lepture/github-cards)
+* [gist-embed] - [Blair Vanderhoof](https://github.com/blairvanderhoof)
+
+### Installation
+1. Download the plugin from github.
+2. Extract the zip file.
+3. Upload the plugin folder to wp-content/plugins/ directory.
+4. active the plugin.
+5. enjoy :)
---
+++ wp-live-code-embed.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Plugin Name: Live Code Embed
+ * Plugin URI: https://waf.co.il/category/internet/wordpress/plugins/live-code-embed
+ * Description: This plugin allow to you embed code snippets from pastebin, github gists, github profile, github repository and codepen.
+ * Version: 1.0.0
+ * Author: Snir Sofer
+ * Author URI: http://waf.co.il
+ * Text Domain: live-code-embed
+ * License: Free
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+wp_enqueue_script( 'wp-live-code-embed-gitcard', '//cdn.jsdelivr.net/github-cards/latest/widget.js', array(),false,true);
+wp_enqueue_script( 'wp-live-code-embed-gists', '//cdnjs.cloudflare.com/ajax/libs/gist-embed/2.7.1/gist-embed.min.js', array(),false,true); //embed gists
+
+/** Short codes **/
+function embed_Pastebin_shortcode($atts)
+{
+    $params = shortcode_atts(['id' => 'BVZJapzw'],$atts);
+    return '<iframe src="https://pastebin.com/embed_iframe/'.$params['id'].'" style="border:none;width:100%"></iframe>';
+}
+add_shortcode( 'pastebin', 'embed_Pastebin_shortcode' );
+
+
+/* Github gists */
+function embed_Github_gists_shortcode($atts)
+{
+    $params = shortcode_atts(['id' => 'b7a79710ac396bd874b1f14be8f5e3b3'],$atts);
+    return '<code data-gist-id="'.$params['id'].'"></code>';
+}
+add_shortcode( 'gist', 'embed_Github_gists_shortcode' );
+
+/* Github User Profile */
+function embed_Github_UserProfile_shortcode($atts)
+{
+    $params = shortcode_atts(['username' => 'SnirSofer','theme' => 'default'],$atts);
+    return '<div class="github-card" data-github="'.$params['username'].'" data-theme="'.$params['theme'].'"></div>';
+}
+add_shortcode( 'github_profile', 'embed_Github_UserProfile_shortcode' );
+
+
+
+/* Github Repository */
+function embed_Github_repo_shortcode($atts)
+{
+    $params = shortcode_atts(['id' => 'SnirSofer/PHP-Snippets','theme' => 'default'],$atts);
+    return '<div class="github-card" data-github="'.$params['id'].'" data-theme="'.$params['theme'].'"></div>';
+}
+add_shortcode( 'github_repo', 'embed_Github_repo_shortcode' );
+
+/* Codepen */
+function embed_Codepen_shortcode($atts)
+{
+    $params = shortcode_atts(['id' => 'evsuw'],$atts);
+    return '<iframe height="265" style="width: 100%;" scrolling="no" src="//codepen.io/wesbos/embed/'.$params['id'].'/?height=265&theme-id=light&default-tab=html,result" frameborder="no" allowtransparency="true" allowfullscreen="true"></iframe>';
+}
+add_shortcode( 'codepen', 'embed_Codepen_shortcode' );
+
